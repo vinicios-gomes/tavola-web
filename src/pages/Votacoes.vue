b7aac06a0d0d8b0093c96616975b73b69f3cf953
Francisco Vinicios Gomes Viana <viniciusgomesviana@gmail.com>
2020-09-26 10:03:02 -0300

    Fiz/Css

--- src/pages/Votacoes.vue
+++ src/pages/Votacoes.vue
@@ -6,12 +6,42 @@
         <button>Em andamento</button>
         <button>Finalizado</button>
       </div>
-      <div class="card">
-        <div>
-          <p>Icone</p>
-          <p>Cidades maravilhosas</p>
+      <div id="ts">
+        <div class="card">
+          <div>
+            <img src="../assets/check-list.svg" />
+            <p>Cidades maravilhosas</p>
+          </div>
+          <button type="button">Votar</button>
         </div>
-        <button type="button">Votar</button>
+        <div class="card">
+          <div>
+            <img src="../assets/check-list.svg" />
+            <p>Cidades maravilhosas</p>
+          </div>
+          <button type="button">Votar</button>
+        </div>
+        <div class="card">
+          <div>
+            <img src="../assets/check-list.svg" />
+            <p>Cidades maravilhosas</p>
+          </div>
+          <button type="button">Votar</button>
+        </div>
+        <div class="card">
+          <div>
+            <img src="../assets/check-list.svg" />
+            <p>Cidades maravilhosas</p>
+          </div>
+          <button type="button">Votar</button>
+        </div>
+        <div class="card">
+          <div>
+            <img src="../assets/check-list.svg" />
+            <p>Cidades maravilhosas</p>
+          </div>
+          <button type="button">Votar</button>
+        </div>
       </div>
     </div>
   </div>
@@ -24,30 +54,59 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#votacoes {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
 h1 {
   padding-bottom: 20px;
   padding-left: 10px;
   border-bottom: 1px solid #000;
   width: 80%;
 }
-#content {
-  width: 90%;
-  height: 80vh;
-  background-color: #cecece;
+.btState {
+  margin-bottom: 60px;
+  width: 100%;
   display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  button {
+    width: 150px;
+    height: 30px;
+    border: none;
+    margin: 10px;
+  }
+}
+#ts {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+}
+#content {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  align-items: center;
   flex-direction: column;
   align-items: initial;
-  padding-left: 10%;
   padding-top: 30px;
 }
 .card {
-  width: 50%;
-  height: 50px;
+  width: 55%;
+  height: 60px;
+  padding: 15px;
   font-size: 20px;
   display: flex;
   flex-direction: row;
-  background-color: rgb(0, 0, 0, 0.2);
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048),
+    0 12.5px 10px rgba(0, 0, 0, 0.06), 0 22.3px 17.9px rgba(0, 0, 0, 0.072);
   align-items: center;
+  margin: 10px;
   justify-content: space-between;
   div {
     display: flex;
@@ -55,13 +114,21 @@ h1 {
     justify-content: space-between;
     width: 250px;
   }
+  img {
+    width: 32px;
+    height: 32px;
+  }
   button {
     border: none;
     background-color: var(--color-primary);
-    width: 60px;
+    width: 78px;
     height: 40px;
     border-radius: 8px;
     color: #fff;
+
+    &:hover {
+      background-color: rgba(0, 70, 101, 0.9);
+    }
   }
 }
 </style>
