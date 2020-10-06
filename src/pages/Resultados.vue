5afe1f59fd2a247ba217e19eaf6d05da9ffad9f0
Francisco Vinicios Gomes Viana <viniciusgomesviana@gmail.com>
2020-10-06 13:12:26 -0300

    Add files via upload

--- src/pages/Resultados.vue
+++ src/pages/Resultados.vue
@@ -5,19 +5,40 @@
     </header>
     <div id="container">
       <div class="cardResult">
-        <h2>Abstenho-me</h2>
+        <header>
+          <img src="../assets/abs.png" />
+          <span>
+            <h2>Abstenho-me</h2>
+          </span>
+        </header>
         <p>{{ votacoes.abstencoes }}</p>
       </div>
       <div class="cardResult">
-        <h2>Aprovar a chapa</h2>
+        <header>
+          <img src="../assets/plusicon.png" />
+          <span>
+            <h2>Aprovar a chapa</h2>
+          </span>
+        </header>
         <p>{{ votacoes.aprovar }}</p>
       </div>
       <div class="cardResult">
-        <h2>Aptos a votar</h2>
-        <p>{{ votacoes.aptos }}</p>
+        <header>
+          <img id="dif" src="../assets/circ.png" />
+          <span>
+            <h2>Aptos a votar</h2>
+          </span>
+        </header>
+        <!--<p>{{ votacoes.aptos }}</p>-->
+        <p>34</p>
       </div>
       <div class="cardResult">
-        <h2>Total de votos</h2>
+        <header>
+          <img src="../assets/plusicon.png" />
+          <span>
+            <h2>Total de votos</h2>
+          </span>
+        </header>
         <p>{{ this.somaTotal }}</p>
       </div>
     </div>
@@ -64,21 +85,49 @@ export default {
   flex-direction: column;
   align-items: center;
   width: 350px;
-  height: 250px;
-  background-color: rgba($color: #cecece, $alpha: 0.5);
+  height: 300px;
+  background-color: rgba($color: #ffffff, $alpha: 1);
+  border: 1px solid rgba($color: #000, $alpha: 0.3);
   margin: 10px;
-
+  border-radius: 15px;
   h2 {
     width: 100%;
     height: 35px;
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: rgba($color: #004767, $alpha: 0.7);
+    margin-top: 17px;
+    //background-color: rgba($color: #004767, $alpha: 0.7);
     margin-bottom: 40px;
   }
   p {
-    font-size: 3rem;
+    font-size: 4rem;
+    margin-top: 20px;
+  }
+  header {
+    display: flex;
+    align-items: center;
+    //justify-content: space-between;
+    width: 100%;
+    margin: 5px;
+    img {
+      width: 80px;
+      height: 80px;
+      margin-left: 8px;
+    }
+    span {
+      display: flex;
+      flex-direction: row;
+      border-bottom: 2px solid rgba($color: #000, $alpha: 0.3);
+      width: 100%;
+      height: 10px;
+      align-items: center;
+      margin-left: 3px;
+    }
+    #dif {
+      border: 5px solid #000;
+      border-radius: 50px;
+    }
   }
 }
 #resultados {
